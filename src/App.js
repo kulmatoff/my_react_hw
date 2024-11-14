c3b28018467d00c78cf2e9b62f94d809809049c7
kulmatoff <bayastansandman@gmail.com>
2024-11-14 10:45:10 +0600

    cool update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import { ProductCard } from "./components/ProductCard";
 
+const products = [
+  {
+    id: 0,
+    name: "Santa",
+    price: 100,
+    category: "Santa Claus",
+    image: "https://www.adbranch.com/wp-content/uploads/coca-cola_for_santa_1950.jpg"
+  },
+  {
+    id: 1,
+    name: "Dead MoroZ",
+    price: 200,
+    category: "Ded Moros",
+    image: "https://chips-journal.ru/files/review/2021-11-15/WlpjAaYBR9iasu4g-SgqUfzHUO_Jjh6H_1240_832.jpg"
+  }
+]
+
 function App() {
-  return <div><ProductCard/></div>
+  return <div class="list-products">
+    {
+      products.map((e) => <ProductCard data={e}/>)
+    }
+  </div>
 }
 
 export default App;
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,7 +1,18 @@
-export function ProductCard() {
-    return (
-        <div>
-            Product Card
-        </div>
-    )
+import "./App.css";
+
+export function ProductCard(props) {
+  const { image, category, name, price } = props.data;
+
+  return (
+    <div class="product">
+      <div class="prodct-upper">
+        <img src={image} alt="" />
+        <p class="product-category">{category}</p>
+        <h3 class="product-name">{name}</h3>
+      </div>
+      <div class="price">
+        <h2 class="price-label">${price}</h2> <button>Add to cart</button>
+      </div>
+    </div>
+  );
 }
